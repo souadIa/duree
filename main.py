21c42c1d3a5c8dd9f0e70067cd658fbab1400779
souadIa <souad.henane.edu@groupe-gema.com>
2023-11-03 16:27:18 +0100

    commit

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 class Durée:
     def __init__(self, heures=0, minutes=0, secondes=0):
         self.heures = abs(heures)
